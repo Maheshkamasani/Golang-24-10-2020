f43055ef9c6c99ec5e5731f697ef0de3c364b139
MAHESWAR REDDY KAMASANI <73190083+Maheshkamasani@users.noreply.github.com>
2020-10-25 14:37:44 +0530

    Add files via upload

---
+++ loop.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 5
+	if num == 9 {
+		fmt.Println("Mahesh")
+	} else if num == 5 {
+		fmt.Println("Madhan")
+
+	} else {
+		fmt.Println("Meena")
+	}
+}
